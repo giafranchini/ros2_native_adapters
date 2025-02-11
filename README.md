5a6582f18ddb21a29e661f9c5a4f2824f8271735
giacomo <giacomo.franchini@polito.it>
2025-02-11 12:42:51 +0100

    Revert "Merge branch 'iron' of https://github.com/roncapat/ros2_native_adapters"
    This reverts commit 2e3b4d5ce927620478111f30bebbfc55c93702ab, reversing
    changes made to c27d04abec23f41ff6ab27d7c8d275209a3ae820.

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 This package provides the following ROS2 type adaptations:
 
-| Adapted Message | Native wrapper | Underlying native class | Header | CMake target |
-|-|-|-|-|-|
-| sensor_msgs/PointCloud2 | StampedPointCloud_PCL | pcl::PointCloud\<PointT\> |PCL.hpp| native_adapters::pcl_adapters |
-| sensor_msgs/Image | StampedImage_CV | cv::Mat |CV.hpp| native_adapters::cv_adapters |
-| nav_msgs/OccupancyGrid | StampedOccupancyGrid_CV | cv::Mat |CV.hpp| native_adapters::cv_adapters |
-| dem_msgs/DigitalElevationMap | StampedDigitalElevationMap_CV | cv::Mat |CV.hpp| native_adapters::cv_adapters |
+| Adapted Message | Native wrapper | Underlying native class | Header |
+|-|-|-|-|
+| sensor_msgs/PointCloud2 | StampedPointCloud_PCL | pcl::PointCloud\<PointT\> |CV.hpp|
+| sensor_msgs/Image | StampedImage_CV | cv::Mat |CV.hpp|
+| nav_msgs/OccupancyGrid | StampedOccupancyGrid_CV | cv::Mat |PCL.hpp|
 
 ## Wrapper philosophy
-Each wrapper class includes at least an `std_msgs::Header` attribute and a native handle.
+Each wrapper class includes at least an std_msgs::Header attribute and a native handle.
 
 ## Examples
 See [here](https://github.com/roncapat/ros2-native-adapters-examples).
--- src/SensorMsgPointCloud2_PCLPointCloud.cpp
+++ src/SensorMsgPointCloud2_PCLPointCloud.cpp
@@ -10,7 +10,7 @@ Copyright 2023 Patrick Roncagliolo
 
 StampedPointCloud_PCL::StampedPointCloud_PCL(const StampedPointCloud_PCL & other)
 {
-  // RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Copy constructor called");
+  RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Copy constructor called");
   // raise(SIGTRAP);
   this->header = other.header;
   this->cloud = other.cloud;
@@ -18,7 +18,7 @@ StampedPointCloud_PCL::StampedPointCloud_PCL(const StampedPointCloud_PCL & other
 
 StampedPointCloud_PCL::StampedPointCloud_PCL(StampedPointCloud_PCL && other)
 {
-  // RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Move constructor called");
+  RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Move constructor called");
   // raise(SIGTRAP);
   this->header = std::move(other.header);
   this->cloud = std::move(other.cloud);
@@ -26,7 +26,7 @@ StampedPointCloud_PCL::StampedPointCloud_PCL(StampedPointCloud_PCL && other)
 
 StampedPointCloud_PCL & StampedPointCloud_PCL::operator=(const StampedPointCloud_PCL & other)
 {
-  // RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Assignment operator called");
+  RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Assignment operator called");
   // raise(SIGTRAP);
   if (this == &other) {return *this;}
   this->header = other.header;
@@ -36,20 +36,19 @@ StampedPointCloud_PCL & StampedPointCloud_PCL::operator=(const StampedPointCloud
 
 uint32_t StampedPointCloud_PCL::width() const
 {
-  return std::visit([](auto && _cloud) {return _cloud.width;}, cloud);
+  return std::visit([](auto && cloud) {return cloud.width;}, cloud);
 }
-
 
 uint32_t StampedPointCloud_PCL::height() const
 {
-  return std::visit([](auto && _cloud) {return _cloud.height;}, cloud);
+  return std::visit([](auto && cloud) {return cloud.height;}, cloud);
 }
 
 bool StampedPointCloud_PCL::has_colors() const
 {
   return std::visit(
-    [](auto && _cloud) {
-      using T = typename std::decay_t<decltype(_cloud)>::PointType;
+    [](auto && cloud) {
+      using T = typename std::decay_t<decltype(cloud)>::PointType;
       return pcl::traits::has_color_v<T>;
     }, cloud);
 }
@@ -68,7 +67,7 @@ void rclcpp::TypeAdapter<StampedPointCloud_PCL,
   const StampedPointCloud_PCL & source,
   sensor_msgs::msg::PointCloud2 & destination)
 {
-  // RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Conversion to message");
+  RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Conversion to message");
   // raise(SIGTRAP);
 
   std::visit([&](auto && cloud) {pointcloud_utils::toROSMsg(cloud, destination);}, source.cloud);
@@ -78,7 +77,7 @@ void rclcpp::TypeAdapter<StampedPointCloud_PCL,
 void rclcpp::TypeAdapter<StampedPointCloud_PCL, sensor_msgs::msg::PointCloud2>::convert_to_custom(
   const sensor_msgs::msg::PointCloud2 & source, StampedPointCloud_PCL & destination)
 {
-  // RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Conversion from message");
+  RCLCPP_WARN(rclcpp::get_logger("PointCloud2 Adapter"), "Conversion from message");
   // raise(SIGTRAP);
 
   bool rgb = false;
